3d9ddb629ed120987cba9d53e21b4f5d9d85bfa5
Filipe Lima Acer <cimofelipe4@gmail.com>
2022-07-19 17:14:33 -0300

    Finalizado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const goodPerson = 'Eita pessoa boa!';
+  return {
+    name,
+    feedback: () => goodPerson,
+  };
+};
 
 module.exports = createStudent;
